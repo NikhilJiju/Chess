a89550759eab64018ef0d0f498399bb29206b071
Nikhil Jiju <njj23@scarletmail.rutgers.edu>
2018-03-09 06:24:27 +0000

    Bishop.java edited online with Bitbucket

--- Chess76/src/chess/Bishop.java
+++ Chess76/src/chess/Bishop.java
@@ -1,7 +1,13 @@
 package chess;
 
 public class Bishop extends Pieces{
-
+	
+	String color; 
+	
+	public Bishop(String color){
+		this.color= color; 
+	}
+	
 	public boolean move(int x, int y, int a, int b){
 		if(Math.abs(x-a)==Math.abs(y-b)){
 			return true; 
