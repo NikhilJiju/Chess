b473650dd3b6bae65a88048bf1d0577aa0b76376
Nikhil Jiju <njj23@scarletmail.rutgers.edu>
2018-03-09 05:59:41 +0000

    Rook.java edited online with Bitbucket

--- Chess76/src/chess/Rook.java
+++ Chess76/src/chess/Rook.java
@@ -2,6 +2,12 @@ package chess;
 
 public class Rook extends Pieces{
 	
+	String color; 
+	
+	public Rook(String color){
+		this.color=color; 
+	}
+	
 	public boolean move(int x, int y, int a, int b){
 		if(x-a==0||y-b==0){
 			return true; 
