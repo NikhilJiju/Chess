b0534707ab21a90b82e52d3611830faad492d902
Nikhil Jiju <njj23@scarletmail.rutgers.edu>
2018-03-09 05:56:46 +0000

    Pawn.java edited online with Bitbucket

--- Chess76/src/chess/Pawn.java
+++ Chess76/src/chess/Pawn.java
@@ -1,7 +1,13 @@
 package chess;
 
 public class Pawn extends Pieces{
-
+	
+	String color; 
+	
+	public Knight(String color){
+		this.color=color; 
+	}
+	
 	public boolean move(int x, int y, int a, int b){
 		return false; 
 	}
