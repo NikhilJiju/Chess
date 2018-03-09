740439967429730fffe7dcab8d2cdad690c974d2
Nikhil Jiju <njj23@scarletmail.rutgers.edu>
2018-03-09 05:53:48 +0000

    King.java edited online with Bitbucket

--- Chess76/src/chess/King.java
+++ Chess76/src/chess/King.java
@@ -1,7 +1,10 @@
 package chess;
 
-public class King extends Pieces{
-
+public class King extends Pieces{ 
+	String color; 
+	public King(String color){
+		this.color= color; 
+	}
 	
 	public boolean move(int x, int y, int a, int b){
 		return false; 
