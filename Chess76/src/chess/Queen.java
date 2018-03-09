e7085f6cca79b65c56a56ba848bd258c8743f10e
Nikhil Jiju <njj23@scarletmail.rutgers.edu>
2018-03-09 05:38:30 +0000

    Queen.java edited online with Bitbucket

--- Chess76/src/chess/Queen.java
+++ Chess76/src/chess/Queen.java
@@ -3,6 +3,12 @@ package chess;
 public class Queen extends Pieces{
 
 	public boolean move(int x, int y, int a, int b){
+		if(Math.abs(x-a)==Math.abs(y-b)){
+			return true; 
+		} 
+		if(x-a==0 || y-b==0){
+			return true; 
+		}
 		return false; 
 	}
 }
