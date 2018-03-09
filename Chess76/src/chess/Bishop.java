4c98ec4a12fb6e418739d7f4559e2e47a94c9234
Nikhil Jiju <njj23@scarletmail.rutgers.edu>
2018-03-09 05:35:47 +0000

    Bishop.java edited online with Bitbucket

--- Chess76/src/chess/Bishop.java
+++ Chess76/src/chess/Bishop.java
@@ -3,6 +3,9 @@ package chess;
 public class Bishop extends Pieces{
 
 	public boolean move(int x, int y, int a, int b){
+		if(Math.abs(x-a)==Math.abs(y-b)){
+			return true; 
+		}
 		return false; 
 	}
 }
