df9ee7c1d5296f7b4515882c3e9f5998b1ec8484
Nikhil Jiju <njj23@scarletmail.rutgers.edu>
2018-03-09 05:56:01 +0000

    Knight.java edited online with Bitbucket

--- Chess76/src/chess/Knight.java
+++ Chess76/src/chess/Knight.java
@@ -1,6 +1,12 @@
 package chess;
 
 public class Knight extends Pieces{
+	
+	String color; 
+	
+	public Knight(String color){
+		this.color=color; 
+	}
 
 	public boolean move(int x, int y, int a, int b){
 		return false; 
