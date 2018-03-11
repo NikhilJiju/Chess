8c6518f0b9f0c017828e8593a2b991acabe00583
Jiju <Jiju@Kottarathil-PC>
2018-03-10 21:18:24 -0500

    Now asks for input from user

--- Chess76/src/chess/Chess.java
+++ Chess76/src/chess/Chess.java
@@ -1,20 +1,36 @@
 package chess;
 
+import java.util.Scanner;
+
 public class Chess {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
 		ChessBoard game= new ChessBoard();  
+		int counter=0; 
 		game.makeBoard();
-		game.printBoard();
-		String[][] board= game.pboard; 
-		for (String[] x : board)
-		{
-		   for (String y : x)
-		   {
-		        System.out.print(y + " ");
-		   }
-		   System.out.println();
+		while(true){
+			game.printBoard();
+			String[][] board= game.pboard; 
+			for (String[] x : board)
+			{
+				for (String y : x)
+				{
+					System.out.print(y + " ");
+				}
+				System.out.println();
+			}
+			System.out.println();
+			if(counter%2==0){
+				System.out.print("White's move: ");
+			}
+			else{
+				System.out.print("Black's move: ");
+			}
+			String move= scanner.nextLine(); 
+			System.out.println();
+			counter++; 
 		}
 	}
 
